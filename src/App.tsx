6a669bab92f6eb84471c16df62c93062fe3904e8
NewAma <newamailnam@gmail.com>
2024-12-07 20:49:44 +0330

    a change

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { retrieveLaunchParams, isMiniAppSupported } from "@telegram-apps/sdk"
 function App() {
   useEffect(
     () => {
-      console.log("hello world")
-      if (isMiniAppSupported()) {
-        console.log("that's great to see you in telegram")
-        console.log("params : ")
-        console.log(retrieveLaunchParams())
-      }
-      else {
-        console.log("dude your not in the telegram !!")
-      }
+      setTimeout(() => {
+        console.log("hello world")
+        if (isMiniAppSupported()) {
+          console.log("that's great to see you in telegram")
+          console.log("params : ")
+          console.log(retrieveLaunchParams())
+        }
+        else {
+          console.log("dude your not in the telegram !!")
+        }
+      }, 4000);
     }, []
   )
 
